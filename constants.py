0147fb85a2e898bbaeb266666860b3befd2f858c
yaraSabry277 <yarasabry277@gmail.com>
2024-05-21 23:41:19 +0300

    first commit

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (43, 45, 66)
+LINE_COLOR = (141, 153, 174)
+CIRC_COLOR = (239, 35, 60)
+CROSS_COLOR = (237, 242, 244)
